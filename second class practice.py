b07b5133cb031ac9ec78c9ed7c206502915a644f
numkunwar <41946098+numkunwar@users.noreply.github.com>
2020-08-07 21:54:54 +0545

    Add files via upload

---
+++ second class practice.py
@@ -0,0 +1,30 @@
+#python 3.7
+"""
+print ("Hello, Dcoder!")
+print("enter your first number")
+inpnum = input()
+print("enter your second number")
+inpnum1 = input()
+print("the addition of two number:", int(inpnum) + int(inpnum1))
+
+
+var1 = input("enter your first number\n")
+
+var2 = input("enter your second number\n")
+
+print("subtraction of two digit",int(var1)-int(var2))
+"""
+#print("subtraction of two numbet is : ",var1 - var2
+#
+mystri = ("num is good boy and he have good human nature")
+#print(len(mystri))
+#print(mystri[0:])
+print(mystri.endswith("nature"))
+print(mystri.isalnum()) # itis false because it contain space bar
+print (mystri.isalpha())# it is false because it dont contain any alpha and numericals
+print(mystri.count("o"))
+print(mystri.capitalize())
+print(mystri.find("have"))
+print(mystri.upper())
+print(mystri.lower())
+print(mystri.replace("good","bad"))
